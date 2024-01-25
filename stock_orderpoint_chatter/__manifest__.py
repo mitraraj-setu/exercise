6837d362180bcc6c2025f89b91974e870437bb28
ishani-setu <ishani.manvar@setuconsulting.com>
2024-01-25 16:03:05 +0530

    [IM][ADD]Stock Orderpoint Chatter files added

---
+++ stock_orderpoint_chatter/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Stock Orderpoint Chatter',
+    'application': True,
+    'depends': ['base', 'mail', 'stock'],
+    'data': [
+        'views/stock_warehouse_orderpoint_views.xml'
+    ]
+}
